7321c74ffdfa8f925e9b76294a8de85c2dc11271
Lucie Becz <luciebecz@yahoo.com>
2017-01-25 21:16:13 -0700

    updated version

--- calculator.rb
+++ calculator.rb
@@ -1,6 +1,64 @@
-puts 'Welcome to the Ruby Calculator'
+def menu
+  puts 'Welcome to Ruby Calculator'
+  first_num 
+end 
 
-# I need to get the left hand for the equation 
-# I need to get the operator for the equation
-# I need to get the right hand for the equation
-# I need to calculate and show the result
+
+def first_num
+  puts 'What is the first number?'
+  @first = gets.chomp
+  if @first != '0' && @first.to_i == 0
+    puts 'Invalid Input. Try again'
+    first_num
+  else
+    @first = @first.to_i
+    operator
+  end
+end
+ 
+
+def operator
+  math = ['+', '-', '*', '/', '**']
+  puts "What is the operator?"
+  @op_input = gets.chomp
+  if math.include? @op_input
+    second_num
+  elsif 
+    puts "Operator #{@op_input} is not supported. I only support #{math}. Please try again"
+    operator
+  end
+   
+end
+
+def second_num 
+  puts 'What is the second number?'
+  @second = gets.chomp
+  if @second != '0' && @second.to_i == 0
+    puts 'Invalid Input. Try again'
+    second
+  else 
+    @second = @second.to_i
+    result
+  end
+end
+
+def result
+  puts 'Calculating...'
+  
+  case @op_input 
+  when '+'
+    @answer = @first + @second
+  when '-'
+    @answer = @first - @second
+  when '*'
+    @answer = @first * @second
+  when '/'
+    @answer = @first / @second
+  when '**'
+    @answer = @first ** @second
+  end
+  puts "the result of #{@first} #{@op_input} #{@second} is #{@answer}"
+  first_num
+end
+
+menu
